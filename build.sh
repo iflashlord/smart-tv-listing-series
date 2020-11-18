7c64b0f7f8697e94a8e55a82952eef3258eec5fc
pooladrag <me@iflashlord.com>
2020-11-18 15:46:08 +0330

    Fast Publish!

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 WORKING_DIR="dist"
+
 if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
 
 mkdir -p dist
-mkdir -p dist/assets
-mkdir -p dist/features/detail
-mkdir -p dist/features/main
-mkdir -p dist/features/splash
-mkdir -p dist/features/footer
-mkdir -p dist/service
-mkdir -p dist/features/list
-mkdir -p dist/utils
-mkdir -p dist/json
-
 
 minify index.html > dist/index.html
 
@@ -26,32 +17,38 @@ minify index.html > dist/index.html
 cp favicon.ico dist/favicon.ico
 minify app.js > dist/app.js
 
-
+mkdir -p dist/assets
 cp -av assets/ dist/assets/
+
+mkdir -p dist/json
 cp -av json/ dist/json/
 
 
-minify ./features/splash/SplashView.js > ./dist/features/splash/splashView.js
-minify ./features/splash/splash.css > ./dist/features/splash/splash.css
+mkdir -p dist/features/detail
+minify features/detail/detailFocus.js > dist/features/detail/detailFocus.js
+minify features/detail/detailView.js > dist/features/detail/detailView.js
+minify features/detail/detail.css > dist/features/detail/detail.css
 
-
-minify ./features/detail/detailFocus.js > ./dist/features/detail/detailFocus.js
-minify ./features/detail/detailView.js > ./dist/features/detail/detailView.js
-minify ./features/detail/detail.css > ./dist/features/detail/detail.css
-
-minify ./features/list/listFocus.js > ./dist/features/list/listFocus.js
-minify ./features/list/listView.js > ./dist/features/list/listView.js
-minify ./features/list/list.css > ./dist/features/list/list.css
+mkdir -p dist/features/list
+minify features/list/listFocus.js > dist/features/list/listFocus.js
+minify features/list/listView.js > dist/features/list/listView.js
+minify features/list/list.css > dist/features/list/list.css
  
-minify ./features/main/mainFocus.js > ./dist/features/main/mainFocus.js
-minify ./features/main/mainView.js > ./dist/features/main/mainView.js
-minify ./features/main/main.css > ./dist/features/main/main.css
+mkdir -p dist/features/main
+minify features/main/mainFocus.js > dist/features/main/mainFocus.js
+minify features/main/mainView.js > dist/features/main/mainView.js
+minify features/main/main.css > dist/features/main/main.css
  
+mkdir -p dist/features/splash
+minify features/splash/SplashView.js > dist/features/splash/splashView.js
+minify features/splash/splash.css > dist/features/splash/splash.css
 
-minify ./features/footer/footerView.js > ./dist/features/footer/footerView.js
-minify ./features/footer/footer.css > ./dist/features/footer/footer.css
+mkdir -p dist/features/footer
+minify features/footer/footerView.js > dist/features/footer/footerView.js
+minify features/footer/footer.css > dist/features/footer/footer.css
  
-minify ./service/DataService.js > ./dist/service/dataService.js
+mkdir -p dist/service
+minify service/DataService.js > dist/service/dataService.js
 
 # Set debug mode on false in dataService 
 #dataservicefile="dist/service/dataService.js"
@@ -61,9 +58,10 @@ minify ./service/DataService.js > ./dist/service/dataService.js
 #sed -i -e  "s/$searchdataservice/$replacedataservice/g" $dataservicefile
 #rm dist/service/dataService.js-e
 
-minify ./utils/keycode.js > ./dist/utils/keycode.js
-minify ./utils/keyEvent.js > ./dist/utils/keyEvent.js
-minify ./utils/scrollbar.js > ./dist/utils/scrollbar.js
+mkdir -p dist/utils
+minify utils/keycode.js > dist/utils/keycode.js
+minify utils/keyEvent.js > dist/utils/keyEvent.js
+minify utils/scrollbar.js > dist/utils/scrollbar.js
 
 find dist/ -name ".DS_Store" -delete
 
